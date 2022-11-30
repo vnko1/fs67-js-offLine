e4fa3f04a91589ace1a702698494d7fbfde2f24a
Andrey Valenko <andreyvalenko@gmail.com>
2022-11-30 11:40:56 +0200

    1

---
+++ js/switch.js
@@ -0,0 +1,21 @@
+const option = 10;
+let message;
+
+switch (option) {
+  case 1:
+    message = 'Вы сможете забрать товар завтра с 12:00 в нашем офисе ';
+    break;
+
+  case 2:
+    message = 'Курьер доставит Ваш заказ завтра с 09:00 до 18:00';
+    break;
+
+  case 3:
+    message = 'Посылка будет отправлена сегодня';
+    break;
+
+  default:
+    message = 'Вам перезвонит менеджер';
+}
+
+console.log(message);
